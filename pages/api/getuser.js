921752cb3bc4568c397b2377d8cffd2a92412157
Pranjal Choudhary <pranjal.choudhary2020@vitstudent.ac.in>
2023-08-02 14:03:17 +0530

    fix : finding user using userId

--- pages/api/getuser.js
+++ pages/api/getuser.js
@@ -7,12 +7,37 @@ import jsonewebtoken from "jsonwebtoken";
 const handler = async (req, res) => {
   if (req.method === "POST") {
     let token = req.body.token;
+    console.log(token);
     const user = jsonewebtoken.verify(token, "ourkey");
-    let dbuser = await User.findOne({ email: user.email });
-    const { name, address, phone, email, pin, gstin, pan, image } = dbuser;
-    res
-      .status(200)
-      .json({ name, address, phone, email, pin, gstin, phone, pan, image });
+    // let dbuser = await User.findOne({ email: user.email });
+    let dbuser = await User.findOne({ _id: user.userid });
+
+    const {
+      name,
+      address,
+      phone,
+      email,
+      pin,
+      gstin,
+      pan,
+      image,
+      metamaskaddress,
+      password,
+    } = dbuser;
+
+    res.status(200).json({
+      name,
+      address,
+      phone,
+      email,
+      pin,
+      gstin,
+      phone,
+      pan,
+      image,
+      password,
+      metamaskaddress,
+    });
   } else {
     res.status(400).json({ success: false, error: "no user found" });
   }
